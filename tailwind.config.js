03e10d38e964a3b3f12124c1c3c8524f19b878b7
DMiyalu <dieudonnemiyalu6@gmail.com>
2022-05-13 12:25:05 +0100

    update header

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       'gray-dark': '#334155',
       'gray': '#a8a8a8',
       'gray-light': '#f8f8f8',
+      'gray-light-.95': 'rgba(248, 248, 248, .95)',
       'white': '#fff',
     },
     fontFamily: {
@@ -22,10 +23,13 @@ module.exports = {
     extend: {
       spacing: {
         '25%': '25%',
+        '30%': '30%',
         '50%': '50%',
         '100%': '100%',
         '100vw': '100vw',
         '55px': '55px',
+        '100px': '100px',
+        '650px': '650px',
       },
       backgroundImage: {
         'hero-pattern': "url('https://media.istockphoto.com/photos/soccer-training-for-young-mixed-race-boys-on-a-football-pitch-picture-id1319012201?k=20&m=1319012201&s=612x612&w=0&h=P7HeQCEEWZKMEpvh-jOA8corCWZMaY0yVips4oE-zdc=')",
