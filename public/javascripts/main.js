315f9fc52432d059e36528b554a94ba9b8f32a77
DMiyalu <dieudonnemiyalu6@gmail.com>
2022-08-07 10:45:11 +0100

    update header

---
+++ public/javascripts/main.js
@@ -0,0 +1,22 @@
+(function(){
+    /* -- Back to top button -- */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 50) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /* -- Mobile nav toggle -- */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+})
